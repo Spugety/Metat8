911212b6f59778b040909acdc6f0d208d3dc196c
Spugety <103894899+Spugety@users.noreply.github.com>
2022-10-26 12:09:52 -0400

    take ii

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const Sequelize = require('./config/connection');
+const app = express();
+const hbs = exphbs.create({});
+const PORT = process.env.PORT || 9999;
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
+
