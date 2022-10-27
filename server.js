2b9e4a2548cf40efe1764e0e8e4edc148d0ac4a7
jesciak <jesciak@yahoo.com>
2022-10-27 00:25:34 -0400

    creating route and model for Today user entry

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ const app = express();
 const hbs = exphbs.create({});
 const PORT = process.env.PORT || 9999;
 
-
+//Handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//Today routes
+app.use('/today', require('./controllers/api/todayRoutes'));
 
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:' + PORT);
