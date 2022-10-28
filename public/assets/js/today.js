370b1736f91cdbeadfd60937e45f69a121bc67f4
sensational.mack <mackn478@gmail.com>
2022-10-27 22:46:36 -0400

    review affiramtions display/autorefresh

--- public/assets/js/today.js
+++ public/assets/js/today.js
@@ -1,1 +1,8 @@
-// code for entering affirmation and journal entries 
+// code for entering affirmation and journal entries
+const messagePlaceHolder = document.getElementById("messageOfTheDay");
+messagePlaceHolder.innerHTML = getMessageOfTheDay();
+const messageTimer = () => {
+  const messagePlaceHolder = document.getElementById("messageOfTheDay");
+  messagePlaceHolder.innerHTML = getMessageOfTheDay();
+};
+setInterval(messageTimer, 5000);
