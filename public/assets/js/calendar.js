f63ee48900149e9aa8ab4c43e8ad2a05248adb87
Wookie Choi <hyunwook2010@gmail.com>
2022-10-27 20:49:51 -0400

    wrote some logic code in calendar.js, added html tags, and added some css

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,5 +1,21 @@
+const currentDate = document.querySelector(".current_date");
+
 let date = new Date();
-let currentMonth = date.getMonth();
-let currentYear = date.getFullYear();
-let currentDay = date.getDay();
-console.log(date.getDate());
+let currentMonth = date.getMonth(); // gives month number in array index (starts at 0)
+let currentYear = date.getFullYear(); // gives 4 digit year number
+
+const arrayMonth = [
+    "January","February","March","April","May","June","July","August","September","October","November","December"
+]
+
+const renderCalendar = () => {
+    let lastDateOfMonth = new Date(currentYear, currentMonth+1, 0).getDate(); // value set to last date of current month/year combination
+    console.log(lastDateOfMonth);
+
+    for (let i = 0; i < lastDateOfMonth; i++) {
+        const element = arrayMonth[i];
+    }
+    
+    currentDate.textContent = `${arrayMonth[currentMonth]} ${currentYear}`;
+}
+renderCalendar();
