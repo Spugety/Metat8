97343a41ce973c67e3e5dcc4cefc3f9b2b0f11c4
jesciak <jesciak@yahoo.com>
2022-10-28 15:51:35 -0400

    added user logout to userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -26,28 +26,47 @@ router.post('/', async (req, res) => {
             req.session.loggedIn = true;
             res.status(200).json(userData);
         });
-    }  
+    }
     catch (err) {
-            console.log(err)
-            res.status(500).json(err)
-        }
-    });
-
+        console.log(err)
+        res.status(500).json(err)
+    }
+});
 // Login user
-router
-
-router.post('/login', async (req,res)=>{
-    try{
+router.post('/login', async (req, res) => {
+    try {
         const userData = await User.findOne({
             where: {
                 email: req.body.email,
                 username: req.body.username,
             },
         });
-        if(!userData){
-            res.status(400).json({message: 'Incorrect Login information. Please try again!'});
+        if (!userData) {
+            res.status(400).json({ message: 'Invalid Login information. Please try again!' });
+            return;
         }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Invalid Login information. Please try again!' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json({ user: userData, message: 'Welcome! You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
     }
-    )
-
+});
+// Logout user
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(()=>{
+            res.status(200).end();
+        });
+    }else {
+        res.status(400).end();
+    }
+});
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Day } = require('../models');
 
 
 //find a user
@@ -18,10 +18,22 @@ router.get('/', async (req, res) => {
         res.status(500).json(err)
     }
 });
-router.get('/profile', async(req, res) => {
+router.get('/profile', async (req, res) => {
     try {
-        const userData = await User.findb({
-
+        const userData = await User.findByPk(req.session.userId,{
+            attributes: { exclude: ['password'] },
+            include: [{ model: Day }],
+        });
+        const user = userData.get({ plain: true });
+        res.render('profile', {
+            ...user,
+            loggedIn: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/profile');
