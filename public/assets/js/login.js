f03bfe59940e99629ecdc8b53654280d41355108
sensational.mack <mackn478@gmail.com>
2022-10-31 13:39:08 -0400

    merge

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,7 +1,1 @@
-//affirmation and journal entries
-// const loginAttempts = 3; 
-// const inputValidation = () => {
-//     const username = document.getElementById("username").vaue;
-//     const userPass = document.getElementById("userPass").value;
-//     if (username == "Reset" && userPass == "apple")
-// }
+
