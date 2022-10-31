680f3e646738642106e6060332685d8104cfe3d9
sensational.mack <mackn478@gmail.com>
2022-10-31 13:56:43 -0400

    accept

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,1 +1,39 @@
+const signupButtonClick = document.getElementById('signupButton');
+const submitLogin = document.getElementById("submitLogin");
+const submitBtn = document.getElementById("submitLogin");
 
+
+signupButtonClick.addEventListener('click', createAccount);
+submitLogin.addEventListener('click', initMotivate);
+
+function createAccount() {
+
+}
+
+function initMotivate() {
+  const userid = document.getElementById("userid").value.trim();
+  const passid = document.getElementById("passid").value.trim();
+
+  console.log(userid);
+  console.log(passid);
+  
+    fetch("/api/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+            username: userid,
+            password: passid,
+        }),
+        headers: {
+            "content-type": "application/json",
+        }
+    })
+      .then((response) => {
+        if(response.ok) {
+            window.location.replace("/dashboard");
+        // If there's an error, log the error
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+}
