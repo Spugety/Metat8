6e7cbf23b0fd92d8ae2e1d47d97c265e4c3a365a
Spugety <103894899+Spugety@users.noreply.github.com>
2022-10-31 05:07:32 -0400

    Add event listener to login button

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -5,3 +5,15 @@
 //     const userPass = document.getElementById("userPass").value;
 //     if (username == "Reset" && userPass == "apple")
 // }
+
+
+ function initModal(element){
+  
+    const modal = document.createElement('div')
+    modal.classList.add('modal')
+  const child = document.createElement('div')
+    child.classList.add('child')
+    child.innerHTML = element
+   modal.appendChild(child)
+    document.body.appendChild(modal)
+  }
