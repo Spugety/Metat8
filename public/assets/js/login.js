0a3f5edaf16735221acd1fd38dbef3b069a60974
sensational.mack <mackn478@gmail.com>
2022-10-31 10:22:57 -0400

    lets see

--- public/assets/js/login.js
+++
@@ -1,7 +0,0 @@
-//affirmation and journal entries
-// const loginAttempts = 3; 
-// const inputValidation = () => {
-//     const username = document.getElementById("username").vaue;
-//     const userPass = document.getElementById("userPass").value;
-//     if (username == "Reset" && userPass == "apple")
-// }
--- public/assets/js/today.js
+++ public/assets/js/today.js
@@ -1,5 +1,5 @@
 // code for entering affirmation and journal entries
-const moodSelector =document.getElementById("moods")
+const moodSelector =document.getElementById("mood") //changed from moods to mood to match jessia's entryhandlebars value
 moodSelector.addEventListener("change", async (event) => {
     
 const selectedMood = event.target.value
