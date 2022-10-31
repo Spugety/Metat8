46c3a3a3d5ab4f137699a6e507d77d1b52f2f365
Wookie Choi <hyunwook2010@gmail.com>
2022-10-30 20:01:43 -0400

    arrow buttons can now scroll through the months and years

--- public/assets/js/calendar.js
+++ public/assets/js/calendar.js
@@ -1,33 +1,39 @@
 const currentDate = document.querySelector(".currentDate");
 const datesClass = document.querySelector(".dates");
+const backBtn = document.querySelector(".arrowLeft");
+const nextBtn = document.querySelector(".arrowRight");
 
 let monthInc = 0; // track which month you're looking at
 let clicked = null; // whatever day you clicked on
 
-let date = new Date();
-let currMonth = date.getMonth(); // gives month number in array index (starts at 0)
-let currYear = date.getFullYear(); // gives 4 digit year number
-
 const weekdaysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const monthsArray = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
-const renderCalendar = () => {
+function renderCalendar() {
+    let date = new Date();
+
+    if(monthInc !== 0) {
+        date.setMonth(new Date().getMonth() + monthInc);
+    }
+
+    let currMonth = date.getMonth(); // gives month number in array index (starts at 0)
+    let currYear = date.getFullYear(); // gives 4 digit year number
+
     const monthfirstDay = new Date(currYear, currMonth, 1);
     const monthTotalDays = new Date(currYear, currMonth+1, 0).getDate(); // next month first date - 1 = this month last date
-    console.log(monthTotalDays);
-    
+
     const firstDateToString = monthfirstDay.toLocaleDateString("en-us", {
         weekday: "long",
         month: "numeric",
         day: "numeric",
         year: "numeric",
     });
-    console.log(firstDateToString);
 
     // splits string into array of 2 halves. [0] is weekday [1] is date
     const disabledDays = weekdaysArray.indexOf(firstDateToString.split(", ")[0]);
-    console.log(disabledDays);
     
+    datesClass.innerHTML = "";
+
     // loop for as long as total days in month + disabled days that are part of previous month
     for(let i = 1; i <= disabledDays + monthTotalDays; i++) {
         // add div tag
@@ -47,11 +53,26 @@ const renderCalendar = () => {
             daySlot.classList.add("disabled");
         }
 
-
-
         datesClass.appendChild(daySlot);
     }
 
-    currentDate.textContent = `${monthsArray[currMonth + monthInc]} ${currYear}`;
+    //current month and year
+    currentDate.innerText = `${monthsArray[currMonth]} ${currYear}`;
 }
+
+function buttonFunction() {
+    backBtn.addEventListener("click", () => {
+        console.log("clicked");
+        monthInc--;
+        renderCalendar();
+    });
+
+    nextBtn.addEventListener("click", () => {
+        console.log("clicked");
+        monthInc++;
+        renderCalendar();
+    });
+}
+
+buttonFunction();
 renderCalendar();
